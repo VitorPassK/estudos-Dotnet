3f931e63a76864f759cd4219263f183b267784d6
Vítor Passeri <vitorpskaluf@hotmail.com>
2026-06-13 14:11:37 -0300

    feat: adição do projeto de alunos

---
+++ Fiap.Web.Alunos/Controllers/ClienteController.cs
@@ -0,0 +1,45 @@
+﻿using Fiap.Web.Alunos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Alunos.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        private List<ClienteModel> _clientes;
+
+        public ClienteController()
+        {
+            _clientes = GerarClientesMocados();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public static List<ClienteModel> GerarClientesMocados()
+        {
+            var clientes = new List<ClienteModel>();
+            for (int i = 1; i <=5; i++) 
+            {
+                var cliente = new ClienteModel
+                {
+                    ClientId = i,
+                    Nome = "Cliente" + i,
+                    Sobrenome = "Sobrenome" + i,
+                    Email = "cliente" + i + "@exemplo.com",
+                    DataNascimento = DateTime.Now.AddYears(-30),
+                    Observacao = "Observação do cliente " + i,
+                    Representante = new RepresentanteModel
+                    {
+                        RepresentanteId = i,
+                        NomeRepresentante = "Representante" + i,
+                        Cpf = "0000000000" + i
+                    }
+                };
+
+                clientes.Add(cliente);
+
+
+            }
+}
---
+++ Fiap.Web.Alunos/Models/ClienteModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Fiap.Web.Alunos.Models
+{
+    public class ClienteModel
+    {
+        public int ClientId { get; set; }
+        public string? Nome { get; set; }
+        public string? Sobrenome { get; set; }
+        public string? Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string? Observacao { get; set; }
+        public int RepresentanteId { get; set; }
+        public RepresentanteModel? Representante { get; set; }
+
+    }
+}
